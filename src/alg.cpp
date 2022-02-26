0a646574dd64bffdf6ca1b0e24dd9afade9b61ec
Водолагин Артём <52680620+Xainqq@users.noreply.github.com>
2022-02-26 16:00:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,54 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool a = true;
+    for (int i = 1; i < value; i++) {
+        if ((value % i == 0) && (i != 1)) {
+            a = false;
+            break; }
+    }
+    return (a);
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int m = 1;
+    int y = 0;
+    int i = 2;
+    while (y < n) {
+        bool a = true;
+        for (int sr = 2; sr < i; sr++) {
+            if (i % sr == 0) {
+                a = false;
+                break; }
+        }
+        if (a) {
+            m = i;
+            y++; }
+        i++; }
+    return(m);
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+int sr = value;
+    while (true) {
+        bool a = true;
+        sr++;
+        for (int i = 2; i < sr; i++) {
+            if (sr % i == 0) {
+                a = false;
+                break; }
+        }
+        if (a) {
+            return(sr);
+            break; }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+  for (uint64_t  i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i; }
+  }
+  return sum;
 }
